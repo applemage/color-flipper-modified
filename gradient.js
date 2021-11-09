466bca77fe5f1e7a8c31e075cd34298afa243117
applemage <applemage@yahoo.com>
2021-11-09 18:09:32 -0500

    simple modified color-flipper js practice

---
+++ gradient.js
@@ -0,0 +1,8 @@
+const hex1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const gradient = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    
+})
