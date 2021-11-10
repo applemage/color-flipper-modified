7465a82adf6e4ded06a4960fa4d180afbc77aec2
applemage <applemage@yahoo.com>
2021-11-10 01:07:49 -0500

    still working on gradient rendering

--- gradient.js
+++ gradient.js
@@ -1,8 +1,30 @@
-const hex1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const hex2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex =[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
 const btn = document.getElementById('btn');
-const gradient = document.querySelector('.color');
+const gradient1 = document.querySelector('.color1');
+const gradient2 = document.querySelector('.color2');
+const body = document.getElementsByTagName('body');
+     
 
 btn.addEventListener('click', function(){
-    
-})
+    let gradientColor1 = '#'
+    for(let i = 0; i < 6; i++){
+        gradientColor1 += hex[getRandomHex()]; 
+    }
+    gradient1.textContent = gradientColor1;
+    let gradientColor2 = '#'
+    for(let j = 0; j < 6; j++){
+        gradientColor2 += hex[getRandomHex()];
+    }
+    gradient2.textContent = gradientColor2;
+});
+
+function getRandomHex(){
+    return Math.floor(Math.random() * hex.length)
+}
+
+let generateGrad = () =>{
+    let color1 = gradientColor1;
+    let color2 = gradientColor2;
+    let angle = Math.floor(Math.random() * 360);
+   document.body.style.backgroundColor = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
+}
